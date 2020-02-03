11a9bcff720d401d5d879d83fd237935d03f7ebf
sujaypujari2216 <36987102+sujaypujari2216@users.noreply.github.com>
2020-02-03 22:59:33 +0530

    Done

---
+++ pf.java
@@ -0,0 +1,149 @@
+<!--portfolio-->
+    <section class="gallery" id="gallery">
+      <div class="container">
+        <div class="heading text-center">
+          <h2>Portfolio</h2>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis dolor sit amet lacus ultricies rutrum. Curabitur vitae fringilla elit. Sed at nunc congue, cursus erat ac, pellentesque eros. Etiam ullamcorper sed lectus sit amet mattis.</h3>
+        </div>
+
+        <div id="grid-gallery" class="grid-gallery">
+          <section class="grid-wrap">
+            <ul class="grid">
+              <li class="grid-sizer"></li>
+              <!-- for Masonry column width -->
+              <li>
+                <figure>
+                  <img src="img/work/1.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/2.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/3.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/4.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/5.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/6.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+
+              <li>
+                <figure>
+                  <img src="img/work/7.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+
+              <li>
+                <figure>
+                  <img src="img/work/8.jpg" alt="" />
+                  <figcaption>
+                    <h3>Vitae fringilla elit</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                  </figcaption>
+                </figure>
+              </li>
+
+            </ul>
+          </section>
+          <!-- // end small images -->
+
+          <section class="slideshow">
+            <ul>
+              <li>
+                <figure>
+                  <img src="img/work/1.jpg" alt="" />
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/2.jpg" alt="" />
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/3.jpg" alt="" />
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/4.jpg" alt="" />
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/5.jpg" alt="" />
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <img src="img/work/6.jpg" alt="" />
+                </figure>
+              </li>
+
+              <li>
+                <figure>
+                  <img src="img/work/7.jpg" alt="" />
+                </figure>
+              </li>
+
+              <li>
+                <figure>
+                  <img src="img/work/8.jpg" alt="" />
+                </figure>
+              </li>
+            </ul>
+            <nav>
+              <span class="icon nav-prev"></span>
+              <span class="icon nav-next"></span>
+              <span class="icon nav-close"></span>
+            </nav>
+            <div class="info-keys icon">Navigate with arrow keys</div>
+          </section>
+          <!-- // end slideshow -->
+        </div>
+        <!-- // grid-gallery -->
+      </div>
+    </section>
